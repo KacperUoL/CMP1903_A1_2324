87089d1ae7c5f61df9e911b5dfe05e3749123af0
Kacper <54560106+KacperUoL@users.noreply.github.com>
2024-02-26 23:43:12 +0000

    Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,14 +8,6 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        private static Random diceRoll = new Random();
-        public int CurrentRoll { get; private set; }
-
-        public int Roll()
-        {
-            CurrentRoll = diceRoll.Next(1, 7);
-            return CurrentRoll;
-        }
     }
 
 }
