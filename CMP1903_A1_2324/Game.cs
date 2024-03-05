60ce50c629600937ec5693748a68d856225a91c2
Kacper <54560106+KacperUoL@users.noreply.github.com>
2024-03-05 23:58:13 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,8 +8,32 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
-        
-        
+        public Die[] Dice { get; private set; } = new Die[3];
+
+        public Game()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Dice[i] = new Die();
+            }
+        }
+
+        public int DiceRoll()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                int roll = Dice[i].Roll();
+                Console.WriteLine($"Die {i + 1} rolled: {roll}");
+                sum += roll;
+            }
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Sum of the rolled dice is: {sum}");
+
+            return sum;
+        }
     }
 
 }
