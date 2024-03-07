8bd9ef0a30e52003b5988527875113d7c87cfc5d
Kacper <54560106+KacperUoL@users.noreply.github.com>
2024-03-07 01:57:49 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -9,29 +9,20 @@ namespace CMP1903_A1_2324
 {
     internal class Testing
     {
-        private Game game = new Game();
+        private Game _game = new Game();
 
         public void DebugProgram()
         {
-            foreach (var die in game.Dice)
+            foreach (var die in _game.Dice)
             {
                 int roll = die.Roll();
                 Debug.Assert(die.CurrentRoll >= 1 && die.CurrentRoll <= 6, "Dice roll out of range");
             }
 
-            int expectedSum = game.DiceRoll(); // Roll the dice and get the expected sum
+            int expectedSum = _game.DiceRoll();
 
-            Debug.Assert(expectedSum == game.Dice.Sum(die => die.CurrentRoll), "Sum of die rolls don't match expected sum");
+            Debug.Assert(expectedSum == _game.Dice.Sum(die => die.CurrentRoll),
+                "Sum of die rolls don't match expected sum");
         }
     }
-
-        
-    /*
-     * This class should test the Game and the Die class.
-     * Create a Game object, call the methods and compare their output to expected output.
-     * Create a Die object and call its method.
-     * Use debug.assert() to make the comparisons and tests.
-     */
-
-    //Method
 }
